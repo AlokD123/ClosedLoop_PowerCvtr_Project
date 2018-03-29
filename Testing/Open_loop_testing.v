9a5ea2129bdcb8e144f85f5d62b08fbf366aef3a
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2018-03-29 11:48:28 -0400

    Added missing freq case
    Also added comments

--- Testing/Open_loop_testing.v
+++ Testing/Open_loop_testing.v
@@ -1,4 +1,5 @@
 //To test open-loop (fully automatic)
+// Note: GPIO_O[32] is on top, [34] is on bottom
 
 module main(clk, GPIO_0)
 	//Inputs (set manually)
@@ -25,13 +26,16 @@ module main(clk, GPIO_0)
 	reg C_1, C_2;			//DPWM output values, before dead-time added
 	reg deadTime1_AndBit, deadTime2_AndBit; //AND bits for generating dead time
 
-	// Initialize to fs=140kHz, duty=0.6
+	
+	//MAIN INITIALIZATION VALUES
 	initial begin
+		// Initialize to fs=140kHz, duty=0.6
 		duty_8b=8'b1001111; //Normalized to maxcount=250 --> dutyCount=150
-		freq_4b=b0110;
+		freq_4b=b0110;		//fs=140kHz
 		adjDutyCycle = 10'b0; // Also initialize actual duty cycle to zero
 	end
 
+	
 	//Set frequency and duty cycle in digital (count) form
 	FreqConverter freqCvtr(freq_4b, clk, maxcount);
 	DutyCycleConverter DCCvtr(duty_8b, clk, freq_4b, duty);
@@ -51,7 +55,8 @@ module main(clk, GPIO_0)
 
 endmodule
 
-
+// Creates PWM signal with two adjustable dead times.
+// Note: SECOND state is C1=1,C2=0 ..... FIRST state is C1=0,C2=1. FIRST STATE COMES BEFORE SECOND STATE
 module DPWM (clk,resetn,EN,maxcount,adjDutyCycle,C_1,C_2);
 	input clk;
 	input resetn; input EN;
@@ -69,31 +74,31 @@ module DPWM (clk,resetn,EN,maxcount,adjDutyCycle,C_1,C_2);
 	
 	always @(posedge clk, negedge resetn)
 		begin
-		   if (!resetn)
+		   if (!resetn)		//If reset...
 				begin
 					C_1<=0;
 					C_2<=0;
 					counter<=0;
 				end
-			else if(!EN) 
+			else if(!EN) 	//If disabled...
 				begin
 					C_1<=0;
 					C_2<=0;
 				end 
 			else
 				begin 
-					if (counter < maxcount) // at every 0.5 seconds, activate 
-						counter = counter + 1;
+					if (counter < maxcount) // Create sawtooth
+						counter = counter + 1;	//count up every 20ns until reaching maxCount
 					else 
-						counter = 0;
-					//Based on required duty cycle in steady-state, toggle switches
+						counter = 0;		// reset upon reaching max (counter==maxcount)
+					//Based on required duty cycle at current time (possibly before steady-state), toggle switches
 					if (counter > adjDutyCycle) 
-						begin
+						begin					//For SECOND state....
 						   C_2<=1'b0;
 							C_1 <= 1'b1;
 						end 
 					else	
-						begin
+						begin					//For FIRST state....
 							C_1<=1'b0;
 							C_2 <= 1'b1;
 						end
@@ -141,17 +146,17 @@ module DutyCycleConverter (duty_8b, clk, frequency, duty);
 	always@ (*)
 		case (frequency)
 		
-			4'b0000: duty = duty_8b*8'b0000_0100;//1000 and freq = 50khz
+			4'b0000: duty = duty_8b*8'b0000_0100;//1000 and freq = 50khz //Don't go beyond ~1000!!!!!!!!!
 			4'b0001: duty = duty_8b*8'b0000_0100;//769 and freq = 65khz
 			4'b0010: duty = duty_8b*8'b0000_0100;//625 and freq = 80khz
 			4'b0011: duty = duty_8b*8'b0000_0100;//526 and freq = 95khz
 			4'b0100: duty = duty_8b*8'b0000_0010;//455 and freq = 110khz
 			4'b0101: duty = duty_8b*8'b0000_0010;//400 and freq = 125khz
-			4'b0110: duty = duty_8b*8'b0000_0010;//357 and freq = 140khz
+			4'b0110: duty = duty_8b*8'b0000_0010;//357 and freq = 140khz //Don't go beyond ~160!!!!!!!!!
 			4'b0111: duty = duty_8b*8'b0000_0010;//322 and freq = 155khz
 			4'b1000: duty = duty_8b*8'b0000_0010;//294 and freq = 170khz
 			4'b1001: duty = duty_8b*8'b0000_0010; //270 and freq = 185khz
-			4'b1010: duty = duty_8b*8'b0000_0001; //250 and freq = 200khz, we should not go beyond 250!!!!!!!!!
+			4'b1010: duty = duty_8b*8'b0000_0001; //250 and freq = 200khz //Don't go beyond ~64!!!!!!!!!
 			
 			default: duty = duty_8b*8'b0000_0100;
 		endcase	
@@ -169,6 +174,7 @@ module FreqConverter (freq_4b, clk, maxcount);
 			4'b0001: maxcount=10'b1100000001;//769 and freq = 65khz
 			4'b0010: maxcount=10'b1001110001;//625 and freq = 80khz
 			4'b0011: maxcount=10'b1000001110;//526 and freq = 95khz
+			4'b0100: maxcount=10'b0111000111;//455 and freq = 110khz
 			4'b0101: maxcount=10'b0110010000;//400 and freq = 125khz
 			4'b0110: maxcount=10'b0101100101;//357 and freq = 140khz
 			4'b0111: maxcount=10'b0101000010;//322 and freq = 155khz
@@ -181,7 +187,7 @@ module FreqConverter (freq_4b, clk, maxcount);
 
 endmodule
 
-
+//Shift register, to create dead time "AND" bits
 module shiftn (binary, w, Clock, bit_value);
 	input [2:0]binary;
 	integer n;
