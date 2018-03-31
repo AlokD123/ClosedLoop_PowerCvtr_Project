0613fad1f92810f5242604bf51324eef3e797780
Alok Deshpande <alokdeshpande@cmail.carleton.ca>
2018-03-31 00:12:44 -0400

    Fixed debug errors for open loop tester

--- Testing/Open_loop_testing.v
+++ Testing/Open_loop_testing.v
@@ -1,7 +1,7 @@
 //To test open-loop (fully automatic)
 // Note: GPIO_O[32] is on top, [34] is on bottom
 
-module main(clk, GPIO_0)
+module main(clk, GPIO_0);
 	//Inputs (set manually)
 	reg [7:0] duty_8b;
 	reg [4:0] freq_4b;
@@ -19,20 +19,19 @@ module main(clk, GPIO_0)
 	output [34:0]GPIO_0;
 
 	//Holders
-	reg [9:0] maxcount;		//Vm for DPWM.............................................. SET in FreqConverter block
-	reg [9:0] duty;			//original duty cycle to set............................... SET in DutyCycleConverter block
-	reg [9:0] adjDutyCycle; //ACTUAL duty cycle over time, accounting for soft start
-	reg C_1, C_2;			//DPWM output values, before dead-time added
-	reg deadTime1_AndBit, deadTime2_AndBit; //AND bits for generating dead time
-	reg softStart;
+	wire [9:0] maxcount;						//Vm for DPWM.............................................. SET in FreqConverter block
+	wire [9:0] duty;							//original duty cycle to set............................... SET in DutyCycleConverter block
+	wire [9:0] adjDutyCycle; 					//ACTUAL duty cycle over time, accounting for soft start
+	wire C_1, C_2;								//DPWM output values, before dead-time added
+	wire deadTime1_AndBit, deadTime2_AndBit; 	//"AND" bits for generating dead time
+	reg softStart;								//Holds soft start state flag
 
 	
 	//MAIN INITIALIZATION VALUES............ Tune values HERE ONLY!
 	initial begin
 		// Initialize to fs=140kHz, duty=0.6
 		duty_8b=8'b1001111; //Normalized to maxcount=250 --> dutyCount=150
-		freq_4b=8'b0110;		//fs=140kHz
-		adjDutyCycle = 10'b0; // Also initialize actual duty cycle to zero
+		freq_4b=8'b0110;	//fs=140kHz
 		softStart = 0; 		//Ignore soft start for testing
 	end
 
@@ -48,11 +47,11 @@ module main(clk, GPIO_0)
 	
 	//Add dead time "AND" bit generation using shift registers
 	shiftn deadTime1(dt1_3b, C_1, clk, deadTime1_AndBit);
-	shiftn deadTime1(dt2_3b, C_2, clk, deadTime2_AndBit);
+	shiftn deadTime2(dt2_3b, C_2, clk, deadTime2_AndBit);
 
 	//Create output signals
-	assign GPIO_0[32] = bit_value1 & C_1;
-	assign GPIO_0[34] = bit_value2 & C_2;
+	assign GPIO_0[32] = deadTime1_AndBit & C_1;
+	assign GPIO_0[34] = deadTime2_AndBit & C_2;
 
 endmodule
 
@@ -66,7 +65,7 @@ module DPWM (clk,resetn,EN,maxcount,adjDutyCycle,C_1,C_2);
 	
 	reg [9:0] counter; //counter for DPWM sawtooth
 	
-	output C_1; output C_2;
+	output reg C_1; output reg C_2;
 	
 	initial begin
 		counter = 10'b0; // Initialize to zero
@@ -115,8 +114,11 @@ module AdjustDuty (clk,softStart,dutyCycle,adjDutyCycle);		//Adjusts duty cycle 
 	input softStart; 		//signal to indicate initial startup
 	input [9:0] dutyCycle;	//original duty cycle to set
 	
-	output [9:0] adjDutyCycle; // Adjusted duty cycle, to set ACTUAL duty cycle (lower during soft start)
-		
+	output reg [9:0] adjDutyCycle; // Adjusted duty cycle, to set ACTUAL duty cycle (lower during soft start)
+	
+	initial begin
+		adjDutyCycle = 10'b0; // Initialize actual duty cycle to zero
+	end
 	
 	always @(*)
 		begin
@@ -126,7 +128,6 @@ module AdjustDuty (clk,softStart,dutyCycle,adjDutyCycle);		//Adjusts duty cycle 
 						adjDutyCycle <= adjDutyCycle + 1;
 					else
 						adjDutyCycle <= dutyCycle;
-					end
 				end
 			else
 				begin 
@@ -138,28 +139,28 @@ module AdjustDuty (clk,softStart,dutyCycle,adjDutyCycle);		//Adjusts duty cycle 
 
 endmodule
 
-module DutyCycleConverter (duty_8b, clk, frequency, duty);
+module DutyCycleConverter (duty_8b, clk, frequency, dutyCount);
 	input clk;
 	input [3:0] frequency;
 	input [7:0] duty_8b;
-	output [9:0] duty;
-
+	output reg [9:0] dutyCount;
+	
 	always@ (*)
 		case (frequency) /// TO DO: saturate duty to maxCount (duty = duty_8b...>maxCount ? maxCount : duty_8b...)
 		
-			4'b0000: duty = duty_8b*8'b0000_0100;//1000 and freq = 50khz //Don't go beyond ~1000!!!!!!!!!
-			4'b0001: duty = duty_8b*8'b0000_0100;//769 and freq = 65khz
-			4'b0010: duty = duty_8b*8'b0000_0100;//625 and freq = 80khz
-			4'b0011: duty = duty_8b*8'b0000_0100;//526 and freq = 95khz
-			4'b0100: duty = duty_8b*8'b0000_0010;//455 and freq = 110khz
-			4'b0101: duty = duty_8b*8'b0000_0010;//400 and freq = 125khz
-			4'b0110: duty = duty_8b*8'b0000_0010;//357 and freq = 140khz //Don't go beyond ~160!!!!!!!!!
-			4'b0111: duty = duty_8b*8'b0000_0010;//322 and freq = 155khz
-			4'b1000: duty = duty_8b*8'b0000_0010;//294 and freq = 170khz
-			4'b1001: duty = duty_8b*8'b0000_0010; //270 and freq = 185khz
-			4'b1010: duty = duty_8b*8'b0000_0001; //250 and freq = 200khz //Don't go beyond ~64!!!!!!!!!
+			4'b0000: dutyCount = duty_8b*8'b0000_0100;//1000 and freq = 50khz //Don't go beyond ~1000!!!!!!!!!
+			4'b0001: dutyCount = duty_8b*8'b0000_0100;//769 and freq = 65khz
+			4'b0010: dutyCount = duty_8b*8'b0000_0100;//625 and freq = 80khz
+			4'b0011: dutyCount = duty_8b*8'b0000_0100;//526 and freq = 95khz
+			4'b0100: dutyCount = duty_8b*8'b0000_0010;//455 and freq = 110khz
+			4'b0101: dutyCount = duty_8b*8'b0000_0010;//400 and freq = 125khz
+			4'b0110: dutyCount = duty_8b*8'b0000_0010;//357 and freq = 140khz //Don't go beyond ~160!!!!!!!!!
+			4'b0111: dutyCount = duty_8b*8'b0000_0010;//322 and freq = 155khz
+			4'b1000: dutyCount = duty_8b*8'b0000_0010;//294 and freq = 170khz
+			4'b1001: dutyCount = duty_8b*8'b0000_0010; //270 and freq = 185khz
+			4'b1010: dutyCount = duty_8b*8'b0000_0001; //250 and freq = 200khz //Don't go beyond ~64!!!!!!!!!
 			
-			default: duty = duty_8b*8'b0000_0100;
+			default: dutyCount = duty_8b*8'b0000_0100;
 		endcase	
 endmodule 
 
@@ -167,9 +168,10 @@ endmodule
 module FreqConverter (freq_4b, clk, maxcount);
 	input clk;
 	input [3:0] freq_4b;
-	output [9:0] maxcount;
-
-	case (freq_4b)
+	output reg [9:0] maxcount;
+	
+	always@ (*)
+		case (freq_4b)
 		
 			4'b0000: maxcount=10'b1111101000;//1000 and freq = 50khz
 			4'b0001: maxcount=10'b1100000001;//769 and freq = 65khz
@@ -191,6 +193,9 @@ endmodule
 //Shift register, to create dead time "AND" bits
 module shiftn (binary, w, Clock, bit_value);
 	input [2:0]binary;
+	input w, Clock;
+	output reg bit_value;
+	
 	integer n;
 	always@ (binary)
 		case (binary)
@@ -204,11 +209,10 @@ module shiftn (binary, w, Clock, bit_value);
 		endcase
 
 
-	input w, Clock;
 
 	reg [4:0]Q;
 	integer k;
-	output bit_value;
+	
 	always @(posedge Clock)
 			begin
 				for (k = 0; k < 4; k = k+1)
